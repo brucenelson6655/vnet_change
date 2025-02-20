3d27ad41372f54e57a6237b69087ed537bb60ece
Bruce Nelson <bruce.nelson@databricks.com>
2025-02-20 01:15:25 -0800

    updates and formatting changes

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 README
 
 Options : (required)
--c private subnet name
--p public subnet name
--v virtual network resource id
--w workspace resource id
--m managed resource group resource id
--l location
--i is NPIP enabled (true or false)
--s subscription id
--x Azure CLI login (optional)
--h command line help
+- -c private subnet name
+- -p public subnet name
+- -v virtual network resource id
+- -w workspace resource id
+- -s subscription id
+- -x Azure CLI login (optional)
+- -h command line help
 
+Simple script for updating the workspace vnet or coverting a managed vnet to vnet injected. 
+
+Needed are : 
+- the workspace's resource id
+- the new or updated virtual network info :
+  - vnet resource id
+  - public subnet name
+  - private subnet name
+- and the subscription id this workspace lives in
--- ch-adb-vnet.sh
+++ ch-adb-vnet.sh
@@ -6,15 +6,12 @@ usage() {
         echo "-p public subnet name"
         echo "-v virtual network resource id"
         echo "-w workspace resource id"
-        echo "-m managed resource group resource id"
-        echo "-l location"
-        echo "-i is NPIP enabled (true or false)"
         echo "-s subscription id"
         echo "-x Azure CLI login (optional)"
         exit
 }
 
-optstring=":hs:i:w:l:m:v:p:c:x"
+optstring=":hs:w:v:p:c:x"
 
 if [ $# -eq 0 ] ; then 
   usage
@@ -36,9 +33,6 @@ while getopts ${optstring} arg; do
       echo $subscription
       az account set --subscription ${subscription}
       ;;
-    l)
-      location=$OPTARG
-      ;;
     v)
       VnetNameID=$OPTARG
       ;;
@@ -47,12 +41,6 @@ while getopts ${optstring} arg; do
       ;;
     c)
       prvSubnet=$OPTARG
-      ;;
-    m)
-      MRGnameID=$OPTARG
-      ;;
-    i)
-      enableNPIP=$OPTARG
       ;;
     x)
       az login
@@ -71,6 +59,42 @@ done
 
 bearerToken=`az account get-access-token | jq .accessToken | sed 's/\"//g'`
 # bearerToken=`az account get-access-token | jq .accessToken | sed 's/^"\|"$//g'`
+
+ws=`curl --location --globoff --request GET 'https://eastus2euap.management.azure.com/'${workSpaceResourceID}'?api-version=2025-02-01-preview' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Bearer '${bearerToken}`
+
+MRGnameID=`echo $ws | jq .properties.managedResourceGroupId  | sed 's/\"//g'`
+location=`echo $ws | jq .location  | sed 's/\"//g'`
+enableNPIP=`echo $ws | jq .properties.parameters.enableNoPublicIp.value  | sed 's/\"//g'`
+
+echo '{
+            "type": "Microsoft.Databricks/workspaces",
+            "name": "'${workspaceName}'",
+            "location": "'${location}'",
+            "sku": {
+                "name": "premium"
+            },
+            "properties": {
+                "managedResourceGroupId": "'${MRGnameID}'",
+                "parameters": {
+                    "customPrivateSubnetName": {
+                        "value": "'${pubSubnet}'"
+                    },
+                    "customPublicSubnetName": {
+                        "value": "'${prvSubnet}'"
+                    },
+                    "customVirtualNetworkId": {
+                        "value": "'${VnetNameID}'"
+                    },
+                    "enableNoPublicIp": {
+                        "value": '${enableNPIP}'
+                    }
+                }
+            }
+        }' | jq
+
+
 
 curl --location --globoff --request PUT 'https://eastus2euap.management.azure.com/'${workSpaceResourceID}'?api-version=2025-02-01-preview' \
 --header 'Content-Type: application/json' \
