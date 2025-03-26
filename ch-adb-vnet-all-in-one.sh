c2b16279597c8d016d0d35f7d49acf2872dec9cb
Bruce Nelson <bruce.nelson@databricks.com>
2025-03-25 18:21:10 -0700

    logic update

--- ch-adb-vnet-all-in-one.sh
+++ ch-adb-vnet-all-in-one.sh
@@ -5,6 +5,7 @@ usage() {
         echo "-w workspace resource id (required)"
         echo "-a API version (defaults to 2025-02-01-preview)"
         echo "-d debug mode"
+        echo "-Z dry run mode" 
         echo "-x Azure CLI login (optional)"
         exit
 }
@@ -20,12 +21,13 @@ usage() {
 # if pip convert to npip az databricks workspace update --resource-group <> --name <> --enable-no-public-ip true
 # add a Nat gateway
 
-optstring=":hw:a:xd"
+optstring=":hw:a:xdZ"
 
 # defaults
 apiVersion='2025-02-01-preview'
 apiEndpoint='https://management.azure.com'
 debugMode=0
+dryrunMode=0
 pubSubnet="public-subnet"
 prvSubnet="private-subnet"
 addressPrefix='10.140.0.0/16'
@@ -54,6 +56,10 @@ while getopts ${optstring} arg; do
       echo "debug"
       debugMode=1
       set -xv
+      ;;
+    Z)
+      echo "Dry Run Mode Set\nNochanges will be applied, for verification purposes only"
+      dryrunMode=1
       ;;
     x)
       az login
@@ -85,30 +91,103 @@ fi
 
 az account set --subscription ${subscription}
 
-convertNPIP() {
-    if [ $enableNPIP == "false" ]
+
+
+pubpip() {
+    # test if public ip exists already 
+        piprid=`az network public-ip show -g ${resourceGrpName} -n ${pipName} | jq .id  | sed 's/\"//g' 2>> ${workSpaceLog}.err`
+        
+        if [ ! -z $piprid ]
+        then
+            echo "pip $pipName exists, we will use this public ip" 
+            read -p "Are you sure? (Y or N): " -n 1 -r
+            echo    # new line
+            if [[ $REPLY =~ ^[Yy]$ ]]
+            then
+                echo "proceeding"
+            else
+                exit
+            fi
+        else
+            echo create public ip 
+            az network public-ip create -g ${resourceGrpName} -n ${pipName} --location ${location} >> ${workSpaceLog}.log 2>> ${workSpaceLog}.err
+            if [[ $? > 0 ]]
+            then 
+                echo error creating public IP - exiting
+                exit
+            fi
+        fi
+}
+
+natgway() {
+ # create nat gateway 
+    #check to see if we have a natgateway attached 
+    pubSubnetid=${VnetNameID}'/subnets/'${pubSubnet}
+    pubnatgw=`az network vnet subnet show --ids ${pubSubnetid} | jq .natGateway.id | sed 's/\"//g' 2>> ${workSpaceLog}.err` 
+
+    if [ ! $pubnatgw == "null" ]
     then
-        # create public ip 
-        az network public-ip create -g ${resourceGrpName} -n ${pipName}  >> ${workSpaceLog}.log 2>> ${workSpaceLog}.err
-        if [[ $? > 0 ]]
-        then 
-            echo error creating public IP - exiting
+        #if so is it attached to the private subnet as well ?
+        prvSubnetid=${VnetNameID}'/subnets/'${prvSubnet}
+        prvnatgw=`az network vnet subnet show --ids ${prvSubnetid} | jq .natGateway.id | sed 's/\"//g' 2>> ${workSpaceLog}.err` 
+        echo $prvnatgw
+
+        if [ ! $prvnatgw == "null" ]
+        then
+            echo "congrats its complete"
+            exit
+        else 
+            #lets attach that to the private sunet as well
+            echo "attaching NAT gateway to private subnet" 
+            az network vnet subnet update -g ${resourceGrpName} --vnet-name ${VnetName} -n ${prvSubnet} --nat-gateway ${natGW}  >> ${workSpaceLog}.log 2>> ${workSpaceLog}.err
+        fi
+        #if we are good here lets exit. 
+        exit
+    fi
+
+    #if not lets see if a natgateway exists
+    natgwid=`az network nat gateway show --resource-group ${resourceGrpName} --name ${natGW} | jq .id | sed 's/\"//g' 2>> ${workSpaceLog}.err`
+    #if it exists - just need to attach it and exit
+    echo NAT GW $natgwid
+    if [ ! -z $natgwid ]
+    then 
+        echo "$natGW exists, we will use this NAT Gateway" 
+        read -p "Are you sure? (Y or N): " -n 1 -r
+        echo    # new line
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            echo "proceeding"
+        else
             exit
         fi
-        # create nat gateway 
+    else
+        #if not letscreate a pip (if it doesn't exist) and then out nat gateway
+        pubpip
+        echo creating NAT gateway
         az network nat gateway create --resource-group ${resourceGrpName} --name ${natGW} --location ${location} --public-ip-addresses  ${pipName}  >> ${workSpaceLog}.log 2>> ${workSpaceLog}.err
         if [[ $? > 0 ]]
         then 
             echo error creating NAT gateway - exiting
             exit
         fi
-        echo "Converting to NPIP"
-        az databricks workspace update --ids ${workSpaceResourceID} --enable-no-public-ip true  >> ${workSpaceLog}.log 2>> ${workSpaceLog}.err
+    fi
         echo "attaching NAT gateway to public subnet" 
         az network vnet subnet update -g ${resourceGrpName} --vnet-name ${VnetName} -n ${pubSubnet} --nat-gateway ${natGW}  >> ${workSpaceLog}.log 2>> ${workSpaceLog}.err
+        echo "attaching NAT gateway to private subnet" 
+        az network vnet subnet update -g ${resourceGrpName} --vnet-name ${VnetName} -n ${prvSubnet} --nat-gateway ${natGW}  >> ${workSpaceLog}.log 2>> ${workSpaceLog}.err
+}
+
+convertNPIP() {
+    if [ $enableNPIP == "false" ]
+    then
+        echo "Converting to NPIP"
+        az databricks workspace update --ids ${workSpaceResourceID} --enable-no-public-ip true  >> ${workSpaceLog}.log 2>> ${workSpaceLog}.err
+        
     else
         echo Workspace is NPIP
     fi
+    
+    natgway
 }
 
 
@@ -135,14 +214,31 @@ enableNPIP=`echo $ws | jq .properties.parameters.enableNoPublicIp.value  | sed '
 
 VnetNameID=`echo $ws | jq .properties.parameters.customVirtualNetworkId.value  | sed 's/\"//g'`
 
+if [$debugMode == 1]
+then
+   echo "Debug Mode : " 
+   echo "NPIP : " $enableNPIP
+   echo "MRG ID : " $MRGnameID
+   echo "Region : " $location
+   echo "VNet ID : " $VnetNameID
+   if [[ $VnetNameID == "null" ]]
+   then 
+        echo "This is a Managed Vnet workspace"
+   fi
+fi
 
 
+if [$dryrunMode == 1] 
+then
+   echo "Dry Run Mode" 
+else 
+   echo "All in One Mode"
+fi
 
-echo "All in One Mode"
 azws=`az resource show --ids ${workSpaceResourceID}`
 if [[ $? > 0 ]]
 then 
-    echo error workspace metadata - exiting
+    echo error getting workspace metadata - exiting
     exit
 fi
 
@@ -166,9 +262,11 @@ then
     echo "This workspace is vnet injected"
     pubSubnet=`echo $azws | jq .properties.parameters.customPublicSubnetName.value | sed 's/\"//g'`
     prvSubnet=`echo $azws | jq .properties.parameters.customPrivateSubnetName.value | sed 's/\"//g'`
+    echo $prvSubnet $pubSubnet
     convertNPIP
     exit
 fi
+
 
 VnetNameID='/subscriptions/'${subscription}'/resourceGroups/'${resourceGrpName}'/providers/Microsoft.Network/virtualNetworks/'${VnetName}
 
