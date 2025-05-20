abacbdb67f10dc0100cb796634d82a31f38c3f1b
Bruce Nelson <bruce.nelson@databricks.com>
2025-05-20 00:26:08 -0700

    Python notebook PEM

---
+++ generate_pem.py
@@ -0,0 +1,83 @@
+# Databricks notebook source
+private_key_pass_raw = "admin1234" # passphrase for encryption .. could also be a keyvault secret
+pempath = "pem" # suggesting UC volume or DBFS
+
+# COMMAND ----------
+
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+import re
+import os
+
+private_key_pass = private_key_pass_raw.encode('utf-8')
+
+
+# Generate a private key
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+)
+
+# Option to encrypt the private key (optional)
+encrypted_pem_private_key = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.BestAvailableEncryption(private_key_pass),
+)
+
+# Generate the public key in PEM format
+pem_public_key = private_key.public_key().public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo,
+)
+
+
+
+# COMMAND ----------
+
+# Option to write keys to files
+with open(pempath + "/rsa_key.p8", "wb") as f:
+    f.write(encrypted_pem_private_key)
+
+with open(pempath + "/rsa_key.pub", "wb") as f:
+    f.write(pem_public_key)
+
+# COMMAND ----------
+
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+import re
+import os
+
+passphr = "admin1234"
+
+with open(pempath + "/rsa_key.p8", "rb") as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=passphr.encode(),
+        backend=default_backend()
+    )
+private_key_pem = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
+).decode("utf-8")
+private_key_hex64 = re.sub(r"-----.*-----|\n", "", private_key_pem)
+
+with open(pempath + "/rsa_key-hex64.pem", "wb") as f:
+    f.write(private_key_hex64.encode('utf-8'))
+
+
+
+
+# COMMAND ----------
+
+print("Private keys written to folder: ",pempath,"\n")
+
+# Print the PEM encoded keys (for demonstration)
+
+print("Private Key (PEM):\n", str(encrypted_pem_private_key.decode("utf-8"))) 
+print("Public Key (PEM):\n", str(pem_public_key.decode("utf-8")))
+print("Hex64 Key (PEM):\n")
+print(private_key_hex64)
